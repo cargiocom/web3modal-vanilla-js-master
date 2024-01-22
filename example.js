b2d016495fa3fbcc165de1201193d9d8c4632e60
Cargio <141646415+cargiocom@users.noreply.github.com>
2024-01-22 00:34:11 -0800

    Update example.js

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 "use strict";
-44444444444
+444444444444
 const Web3Modal = window.Web3Modal.default;
 const WalletConnectProvider = window.WalletConnectProvider.default;
 const Fortmatic = window.Fortmatic;
